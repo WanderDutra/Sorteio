6253d5151b3446d4aa37ad058cc35d1492580889
wanderDutra <wdutra.desenvolvedor@gmail.com>
2023-09-03 15:16:47 -0300

    atualizaçao java

--- js.js
+++ js.js
@@ -5,12 +5,19 @@ function goodLuck(){
     const nunberMin = Math.ceil(document.querySelector(".min").value)
     const nunberMax = Math.floor(document.querySelector(".max").value)
  
-    
- 
-   const result = Math.floor(Math.random() * (nunberMax - nunberMin + 1)) + nunberMin;
+   if( nunberMin>=nunberMax){
+alert("O valor minimo tem que ser maior que o valor maximo")
+}
+else{
+  const result = Math.floor(Math.random() * (nunberMax - nunberMin + 1)) + nunberMin;
       
-   resultGenerate.innerHTML = result 
+  resultGenerate.innerHTML = result 
 
+
+
+   }
+ 
+  
  } 
  function ChangeResult(){
     const resultGenerate = document.querySelector("#result-text")
