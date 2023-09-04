5746bf766571a5257093f88af50c044795cf53f8
wanderDutra <wdutra.desenvolvedor@gmail.com>
2023-09-04 01:13:38 -0300

    atualizaçao alert

--- js.js
+++ js.js
@@ -1,34 +1,34 @@
 const generate = document.querySelector(".prize-draw")
 const resultGenerate = document.querySelector("#result-text")
 
-function goodLuck(){
-    const nunberMin = Math.ceil(document.querySelector(".min").value)
-    const nunberMax = Math.floor(document.querySelector(".max").value)
- 
-   if( nunberMin>=nunberMax){
-alert("O valor minimo tem que ser maior que o valor maximo")
-}
-else{
-  const result = Math.floor(Math.random() * (nunberMax - nunberMin + 1)) + nunberMin;
-      
-  resultGenerate.innerHTML = result 
+function goodLuck() {
+  const nunberMin = Math.ceil(document.querySelector(".min").value)
+  const nunberMax = Math.floor(document.querySelector(".max").value)
 
+  if (nunberMin >= nunberMax) {
+    alert("O valor minimo tem que ser MENOR que o valor maximo")
+  }
+  else {
+    const result = Math.floor(Math.random() * (nunberMax - nunberMin + 1)) + nunberMin;
 
-
-   }
- 
-  
- } 
- function ChangeResult(){
-    const resultGenerate = document.querySelector("#result-text")
-    
     resultGenerate.innerHTML = result
-    
-    
-    
-    }
+
+
+
+  }
+
+
+}
+function ChangeResult() {
+  const resultGenerate = document.querySelector("#result-text")
+
+  resultGenerate.innerHTML = result
+
+
+
+}
 
 
 
 resultGenerate.addEventListener("change", ChangeResult)
-  generate.addEventListener("click" , goodLuck)
+generate.addEventListener("click", goodLuck)
